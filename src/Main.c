eee930160b712072d9a513c8bb68876995519796
C-Reaper <gnu.c.reaper@gmail.com>
2025-10-31 17:15:17 +0100

    updated

--- src/Main.c
+++ src/Main.c
@@ -1,7 +1,9 @@
 #if defined(__linux__) && !defined(_WIN32)
     #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+    #include "/home/codeleaded/System/Static/Library/StdFont.h"
 #elif defined(_WIN32) || defined(_WIN64)
     #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+    #include "/home/codeleaded/System/Static/Library/StdFont.h"
     //#include "F:/home/codeleaded/System/Static/Library/OMML.h"
     //#include "F:/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
 #elif defined(__APPLE__)
@@ -12,18 +14,20 @@
 
 
 
+StdFont stdfont;
+
 void Setup(AlxWindow* w){
-    
+    stdfont = StdFont_New();
 }
 void Update(AlxWindow* w){
     
 
     Clear(BLACK);
 
-    
+    StdFont_Render_CStr(WINDOW_STD_ARGS,&stdfont,0.0f,0.0f);
 }
 void Delete(AlxWindow* w){
-    
+    StdFont_Free(&stdfont);
 }
 int main(){
     if(Create("Font Renderer",1900,1000,1,1,Setup,Update,Delete))
